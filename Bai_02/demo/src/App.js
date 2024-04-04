b301806fba0febc52ea87fc024f38448e0fe9a0d
officerk2077 <wwwkienmario123@gmail.com>
2024-04-04 15:36:08 +0700

    Bai 01

---
+++ Bai_02/demo/src/App.js
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+import './App.css';
+function App() {
+  return (
+    <section classname="App">
+      <h1>Demo JSX</h1>
+      {/* function component demo */}
+      <Func_JSX_NguyenTrucKien />
+    </section>
+  );
+}
+
+export default App;
