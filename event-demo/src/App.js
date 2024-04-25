91696a56c1f791fcb39bd053e473da4012561e6d
officerk2077 <wwwkienmario123@gmail.com>
2024-04-25 16:12:31 +0700

    lesson04

--- event-demo/src/App.js
+++ event-demo/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import NTK_EventForm1 from './components/NTK_EventForm1'
 import NTK_EventForm2 from './components/NTK_EventForm2';
+import NTK_EventForm3 from './components/NTK_EventForm3';
 
 export default class App extends Component {
   render() {
@@ -9,6 +10,7 @@ export default class App extends Component {
         <h1>Event Form Demo</h1>
         <NTK_EventForm1 />
         <NTK_EventForm2 />
+        <NTK_EventForm3 />
       </div>
     );
   }
--- event-demo/src/components/NTK_EventForm3.js
+++ event-demo/src/components/NTK_EventForm3.js
@@ -9,10 +9,18 @@ export default class NTK_EventForm3 extends Component {
             job:"Student"
         }
     }
+    // Hàm xử lý sự kiện
+    handlechangeName = (ev)=>{
+        this.setState({
+            name:"K22CNT4-ReactsJs"
+        })
+    }
   render() {
     return (
-      <div>
-        
+      <div className='alert alert-primary'>
+            <h2>Thay đổi dữ liệu trong state</h2>
+            <p>Dữ liệu: {this.state.name} - {this.state.job}</p>
+            <button onClick={this.handlechangeName}>Thay đổi name</button>
       </div>
     )
   }
