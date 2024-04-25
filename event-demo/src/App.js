945977c7d4bdc273e0369ab63be61a929616cef6
officerk2077 <wwwkienmario123@gmail.com>
2024-04-25 22:22:43 +0700

    Lesson04

--- event-demo/src/App.js
+++ event-demo/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import NTK_EventForm1 from './components/NTK_EventForm1'
 import NTK_EventForm2 from './components/NTK_EventForm2';
 import NTK_EventForm3 from './components/NTK_EventForm3';
+import NTK_EventForm4 from './components/NTK_EventForm4';
 
 export default class App extends Component {
   render() {
@@ -11,6 +12,7 @@ export default class App extends Component {
         <NTK_EventForm1 />
         <NTK_EventForm2 />
         <NTK_EventForm3 />
+        <NTK_EventForm4 name="Nguyễn Trực Kiên" />
       </div>
     );
   }
--- event-demo/src/components/NTK_EventForm4.js
+++ event-demo/src/components/NTK_EventForm4.js
@@ -3,12 +3,24 @@ import React, { Component } from 'react'
 export default class NTK_EventForm4 extends Component {
     constructor(props){
         super(props)
+        this.state={
+            name:"Kiên Kiên"
+        }
+    }
+
+    handleGetName = ()=>{
+      alert(this.props.name);
+      this.setState({
+          name:this.props.name
+      })
     }
   render() {
     return (
-      <div>
-        
+      <div className='alert alert-info'>
+            <h2>Lấy dữ liệu từ props</h2>
+            <button onClick={this.handleGetName}>Lấy tên</button>
+            <h2>Welcome to , {this.state.name}</h2>
       </div>
-    )
+    );
   }
 }
