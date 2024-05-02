953a1f500746298ffaff7e974ec57ecb6b0c4e8c
officerk2077 <wwwkienmario123@gmail.com>
2024-05-02 20:35:07 +0700

    Lesson05

---
+++ Bai_05/ntk-event-form/src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Controlled Component ReactJs Form</h1>
+        <ntk_form1/>
+      </div>
+    );
+  }
+}
