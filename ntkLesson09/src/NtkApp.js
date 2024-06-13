25dc51121333291129f0040210bd2a54c5e21f34
kiên trực <wwwkienmario123@gmail.com>
2024-06-13 15:15:33 +0700

    Lesson09

---
+++ ntkLesson09/src/NtkApp.js
@@ -0,0 +1,16 @@
+import {React} from 'react';
+import './App.css';
+
+function NtkApp() {
+
+  // Sử dụng hàm useState của hook
+  return (
+    <div className="container border mt-5 p-3">
+      <h1 className='text-center my-3'>Xử lý chức năng CRUD - Hook - API</h1>
+      <hr/>
+
+    </div>
+  );
+}
+
+export default NtkApp;
